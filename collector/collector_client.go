0aed2b84d8941e41201f50311b2ac9bbde5173d0
李远君 <1123431949@qq.com>
2019-05-17 14:36:07 +0800

    Update collector_client.go

--- collector/collector_client.go
+++ collector/collector_client.go
@@ -9,6 +9,7 @@ package collector
 import "github.com/prometheus/common/log"
 
 const (
+	// export name
 	ExporterName = "service_exporter"
 )
 
@@ -22,13 +23,13 @@ func (sc *ServiceClient) data(dataSource map[string]interface{}) map[string]inte
 	return dataSource
 }
 
-// Exporter实现了prometheus.Collector interface, 指标出口
+// Exporter 实现了prometheus.Collector interface, 指标出口
 type Exporter struct {
 	// 参数为服务客户端ServiceClient的结构体指针，来调用client的数据接口
 	client *ServiceClient
 }
 
-// ServiceOpts表示ServiceClient配置项，初始化Exporter时使用
+// ServiceOpts 表示ServiceClient配置项，初始化Exporter时使用
 type ServiceOpts struct {
 	Name string
 }
